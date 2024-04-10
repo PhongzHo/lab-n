e71cf6f5ad8539347a76df3da738c2d4086ae925
Phong <hothanhphong0806@gmail.com>
2024-04-11 00:44:08 +0700

    first commit

---
+++ src/modules/home/footer/components/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='h-[30px] md:h-[60px] bg-slate-300 font-medium'>Footer</div>
+  )
+}
---
+++ src/modules/home/nav/components/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Nav() {
+    return (
+        <div className='md:h-[80px] h-[40px] bg-red-200 font-extrabold'>Nav</div>
+    )
+}
